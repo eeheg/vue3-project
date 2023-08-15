0ba7425db3c36d0d40376a5b323717625b03d815
eeheg <sjh8924@naver.com>
2023-08-16 01:59:33 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <TodoSimpleForm @add-todo="addTodo" />
+
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
+    <div 
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2 d-flex align-tems-center">
+        <div class="form-check flex-grow-1">
+          <input class="form-check-input" type="checkbox" v-model="todo.completed">
+          <label class="form-check-label" :class="{todo : todo.completed}">{{ todo.subject }}</label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
+        </div>
+      </div>  
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  componetns: {
+    TodoSimpleForm
+  },
+
+  setup() {
+    
+    const todos = ref([]);
+
+    const addTodo = (todo) => {
+      console.log(todo);
+      todos.value.push(todo);
+    }
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    }
+
+    return {
+      addTodo,
+      todos,
+      deleteTodo
+    };
+
+}}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
